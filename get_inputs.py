6d98713ab2121a54e8df33e1d8856de6ad9b0977
omololevy <cotechlevy@gmail.com>
2022-08-25 16:33:56 +0300

    Adjust the inputs to capture time and location

--- get_inputs.py
+++ get_inputs.py
@@ -22,7 +22,7 @@ def get_inputs(args):
 		LA = -1.286389 #Latitude, decimal degrees -90 to 90
 		LO = 36.817223 #Longitude, decimal degrees -180 to 180 
 		city = 'Nairobi, KE'
-		print('Using default location: {city}, Lat: {LA}, Lon: {LO}')
+		print(f'Using default location: {city}, Lat: {LA}, Lon: {LO}')
 	else:
 		city = args.city
 		geolocator = geocoders.Nominatim(user_agent='Sun')  #geopy city->LA,LO locator
@@ -32,7 +32,28 @@ def get_inputs(args):
 			exit()
 		LA = location.latitude
 		LO = location.longitude
-		print('Using city input:', location)
+		print(f'Using city input {location}')
 		print(f'Lat: {LA}', 'Lon: {LO}')
 
+	# get timezone from LA and LO
+	tz = tzwhere.tzwhere(forceTZ=True)  #initialize timezone finder 
+	timezone = pytz.timezone(tz.tzNameAt(LA, LO, forceTZ=True))
+
+	#date and time input
+	if args.date_time == None:  #if no time provided, get the current in the timezone of interest
+		date_time_local = dt.datetime.now(timezone)    
+		print(f'Using default time (current time at location) {date_time_local}')
+	else:
+		try: 
+			date_time = dt.datetime.strptime(args.date_time, '%Y-%m-%d %H:%M:%S')
+			date_time_local = timezone.localize(date_time)
+			print(f'Using time input {date_time_local}')
+		except:
+			print('Error with time input, ensure format is %Y-%m-%d %H:%M:%S')
+			exit()
+
 	
+	print(f'Using timezone {timezone}')
+	   #attach timezone to date_time making it aware
+
+	return date_time_local, LA, LO, city
